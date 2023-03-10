8eb048df9f8b19869db5f3ddc2c906e0c10ae287
dbgksquf05 <dbgksquf05@naver.com>
2023-03-10 10:01:50 +0900

    S08P22A706-153 feat: router 및 page 구조 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Routers from "./Routers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RecoilRoot>
+        <BrowserRouter>
+          {/* <NavBar /> */}
+          <Routers />
+        </BrowserRouter>
+      </RecoilRoot>
     </div>
   );
 }
