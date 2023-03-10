6c105d29f1fd93553fa37c607f1f142f9f3175e7
dbgksquf05 <dbgksquf05@naver.com>
2023-03-10 18:03:08 +0900

    S08P22A706-166 feat: feature toggle 기능

--- frontend/src/Routers.tsx
+++ frontend/src/Routers.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
+      <Route path="admin" element={ <Admin /> } />
     </Routes>
   )
 }
