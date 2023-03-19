56ce15bbbdbe5c6b179febc375ce06e70dd873e6
hanbeulYou <hanbeul.you@gmail.com>
2023-03-20 00:54:34 +0900

    S08P22A706-166 fix: dropdown props 수정

--- frontend/src/components/common/DropDown/DropDownItem.tsx
+++ frontend/src/components/common/DropDown/DropDownItem.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import tw, { styled, css, TwStyle } from 'twin.macro';
 
 interface DropDownItemProps {
-  key: number;
   content: string;
   setSelected: React.Dispatch<React.SetStateAction<string>>;
   setIsClicked: React.Dispatch<React.SetStateAction<boolean>>;
@@ -20,12 +19,13 @@ const onClick = ({content, setSelected, setIsClicked}: onClickProps) => {
 }
 
 const DropDownItemBtn = styled.button((props: { isHovering: boolean }) => [
+  tw`w-full h-14 rounded flex justify-between items-center`,
   props.isHovering === true
-    ? tw`w-full h-14 rounded flex justify-between items-center bg-dark-primary-var px-4 text-main-bold text-black`
-    : tw`w-full h-14 rounded flex justify-between items-center bg-dark-evaluated px-4 text-main text-dark-primary`
+    ? tw`bg-dark-primary-var px-4 text-main-bold text-black`
+    : tw`bg-dark-evaluated px-4 text-main text-dark-primary`
 ])
 
-const DropDownItem = ({key, content, setSelected, setIsClicked}: DropDownItemProps) => {
+const DropDownItem = ({content, setSelected, setIsClicked}: DropDownItemProps) => {
   const [isHovering, setIsHovering] = useState(false);
 
   return (
