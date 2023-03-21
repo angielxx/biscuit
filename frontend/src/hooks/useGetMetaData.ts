7c8df94ab5d587055950b330ade0ad4c57734e39
angleixx <angielxx94@gmail.com>
2023-03-22 01:17:51 +0900

    S08P22A706-301 feat: 메타데이터 스크랩 기능 구현 완료

---
+++ frontend/src/hooks/useGetMetaData.ts
@@ -0,0 +1,44 @@
+function getProtocol(url: string) {
+  let end = url.indexOf('://') + 3;
+  return url.slice(0, end);
+}
+
+function getHostname(url: string) {
+  let start = url.indexOf('://') + 3;
+  let end = url.indexOf('/', start);
+  return url.slice(start, end);
+}
+
+function scrap(doc: Document, url: string) {
+  // 이미지
+  let imageEl = doc.querySelector("meta[property='og:image']");
+  let image = imageEl ? imageEl.getAttribute('content') : null;
+
+  if (!image) {
+    imageEl = doc.querySelector('img');
+    image = imageEl ? imageEl.getAttribute('src') : null;
+
+    if (image && image.indexOf('http') === 0) {
+    } else if (image && image[0] === '/') {
+      image = getProtocol(url) + getHostname(url) + image;
+    } else {
+      image = '';
+    }
+  }
+
+  // 글요약본
+  let descEl = doc.querySelector("meta[property='og:description']");
+  let desc = descEl ? descEl.getAttribute('content') : '';
+  return { image, desc };
+}
+
+export const useGetMetaData = async (url: string) => {
+  const html = await fetch(url, {
+    mode: 'cors',
+  }).then((res) => res.text());
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const { image, desc } = scrap(doc, url);
+  console.log(desc);
+  return { image, desc };
+};
