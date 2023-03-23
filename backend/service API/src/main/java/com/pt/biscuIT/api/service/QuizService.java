8edd4375580414ada758d44d43d966859ac3f4b6
이지영 <jylisa0612@naver.com>
2023-03-23 13:17:22 +0900

    chore: .idea파일 삭제

--- backend/service API/src/main/java/com/pt/biscuIT/api/service/QuizService.java
+++
@@ -1,9 +0,0 @@
-package com.pt.biscuIT.api.service;
-
-import com.pt.biscuIT.api.dto.quiz.ProvideQuizDto;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface QuizService {
-	ProvideQuizDto provideQuiz(Long contentId);
-}
--- backend/service API/src/main/java/com/pt/biscuIT/db/repository/MemberBookmarkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pt.biscuIT.db.repository;
-
-import com.pt.biscuIT.db.entity.MemberBookmark;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberBookmarkRepository extends JpaRepository<MemberBookmark, Long> {
-}
