45dfd2d1bebcd91c764ed0d88ab294280fed102c
이지영 <jylisa0612@naver.com>
2023-03-23 13:17:22 +0900

    chore: .idea파일 삭제

--- backend/service API/src/main/java/com/pt/biscuIT/db/entity/SocialDomain.java
+++
@@ -1,5 +0,0 @@
-package com.pt.biscuIT.db.entity;
-
-public enum SocialDomain {
-	GOOGLE, GITHUB
-}
--- backend/service API/src/main/java/com/pt/biscuIT/db/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pt.biscuIT.db.repository;
-
-import com.pt.biscuIT.db.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
