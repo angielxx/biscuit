8c57a713cba08341d2a66f497a88959d62f8834a
7unho <rlawnsgh8395@naver.com>
2023-03-24 10:49:56 +0900

    feat: GlobalExceptionHandler 구조 작성

--- backend/service API/src/main/java/com/pt/biscuIT/common/exception/GlobalExceptionHandler.java
+++ backend/service API/src/main/java/com/pt/biscuIT/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pt.biscuIT.common.exception;
 
 
 import com.pt.biscuIT.common.model.response.BaseResponseBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({BiscuitException.class})
     protected ResponseEntity handleBiscuitException(BiscuitException e) {
-        return new ResponseEntity(BaseResponseBody.of(e.getErrorCode().getStatus().value(), e.getErrorCode().getMessage()), e.getErrorCode().getStatus());
+        return new ResponseEntity(BaseResponseBody.of(e.getErrorCode().getStatus().value(), e.getErrorCode().getMessage()), HttpStatus.valueOf(e.getErrorCode().getStatus().value()));
+    }
+
+    @ExceptionHandler({Exception.class})
+    protected ResponseEntity handleException(Exception e) {
+        return new ResponseEntity(BaseResponseBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
