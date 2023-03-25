0403728ea5ff084024587c77f85a7df56fae8323
hanbeulYou <hanbeul.you@gmail.com>
2023-03-25 11:38:16 +0900

    S08P22A706-311 feat: modal 창 오픈 로직 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,10 @@ import Navbar from './components/common/Navbar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './styles/App.css';
-// import { isModalOpenState } from './recoils/Contents/Atoms';
-// import FeedbackModal from './components/common/Modal/FeedbackModal';
 
 const queryClient = new QueryClient();
 
 function App() {
-  // const [isModalOpen, setIsModalOpen] = useRecoilState(isModalOpenState);
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
@@ -19,9 +16,6 @@ function App() {
           <BrowserRouter>
             <Navbar />
             <div className="mt-20">
-              {/* {isModalOpen
-                ? <FeedbackModal onClose={() => setIsModalOpen(false)}/>
-                : null} */}
               <Routers />
             </div>
           </BrowserRouter>
--- frontend/src/Routers.tsx
+++ frontend/src/Routers.tsx
@@ -4,13 +4,24 @@ import Admin from './pages/Admin';
 import Search from './pages/Search';
 import Category from './pages/Category';
 
+import { useRecoilState } from 'recoil';
+import { isModalOpenState } from './recoils/Contents/Atoms';
+import FeedbackModal from './components/common/Modal/FeedbackModal';
+
 export default function Routers() {
+  const [isModalOpen, setIsModalOpen] = useRecoilState(isModalOpenState);
+
   return (
+    <div>
+    {isModalOpen
+      ? <FeedbackModal onClose={() => setIsModalOpen(false)}/>
+      : null}
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
       <Route path="/category/:name" element={<Category />} />
       <Route path="admin" element={<Admin />} />
     </Routes>
+    </div>
   );
 }
