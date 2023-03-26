b913ff41e379e1e2f187929ddf4bdf60ac745590
angleixx <angielxx94@gmail.com>
2023-03-26 11:13:44 +0900

    S08P22A706-301 design: 홈 화면 깨지는 문제 해결

--- frontend/src/components/Home/HomeContents.tsx
+++ frontend/src/components/Home/HomeContents.tsx
@@ -1,32 +1,26 @@
-import tw, { styled, css } from "twin.macro";
-import HomeContentList from "./HomeContentList";
+import tw, { styled, css } from 'twin.macro';
+import HomeContentList from './HomeContentList';
 
 const HomeContentsContainer = tw.div`
   flex-col w-full h-fit
 `;
 
-
 const HomeContents = ({}: Object) => {
-  const unAuthContents = ["popular", "recent", "category"];
-  const authContent = ["fit", "favor_category", "bookmarked", "similar_member"];
+  const unAuthContents = ['popular', 'recent', 'category'];
+  const authContent = ['fit', 'favor_category', 'bookmarked', 'similar_member'];
   const isAuth = false;
-  
+
   return (
     <HomeContentsContainer>
-      { isAuth 
+      {isAuth
         ? authContent?.map((category) => {
-          return (
-            <HomeContentList category={category} />
-          )
-        })
+            return <HomeContentList category={category} />;
+          })
         : unAuthContents?.map((category) => {
-          return (
-            <HomeContentList category={category} />
-          )
-        })
-      }
+            return <HomeContentList category={category} />;
+          })}
     </HomeContentsContainer>
   );
-}
+};
 
 export default HomeContents;
