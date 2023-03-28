24c0cb57d7dd729f65ed78b12c7f9162ca0fc1e3
randallkk <randallkk.sw@gmail.com>
2023-03-28 14:14:27 +0900

    S08P22A706-219 fix: erased duplicated ContentController method feedbackContent

--- backend/service API/src/main/java/com/pt/biscuIT/api/controller/ContentController.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/ContentController.java
@@ -1,26 +1,12 @@
 package com.pt.biscuIT.api.controller;
 
-import com.pt.biscuIT.api.dto.content.ContentInfoDto;
-import com.pt.biscuIT.api.response.RandomRecentContentRes;
 import com.pt.biscuIT.api.service.ContentService;
-import com.pt.biscuIT.api.service.RecommandService;
-import com.pt.biscuIT.common.exception.BiscuitException;
-import com.pt.biscuIT.common.exception.ErrorCode;
-import com.pt.biscuIT.common.model.response.BaseResponseBody;
-import com.pt.biscuIT.common.model.response.PageMetaData;
 import com.pt.biscuIT.common.util.CsvUtil;
 import com.pt.biscuIT.db.repository.ContentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/contents")
@@ -37,7 +23,7 @@ public class ContentController {
         contentService.getContentDetail(contentId);
     }
 
-    @PostMapping("/{contentId}/feedback")
+    @PostMapping("/{contentId}/feedbacks")
     public ResponseEntity<?> feedbackContent(
             @PathVariable Long contentId,
             @RequestParam String feedback,
@@ -49,27 +35,4 @@ public class ContentController {
         return ResponseEntity.ok("SUCCESS");
     }
 
-    @PostMapping("/{contentId}/feedback")
-    public ResponseEntity<?> feedbackContent(
-            @PathVariable Long contentId,
-            @RequestParam String feedback,
-            @RequestParam(required = false) String timecost
-    ) {
-//        contentService.feedbackContent(contentId, feedback);
-        CsvUtil csvUtil = new CsvUtil();
-        csvUtil.writeCsvFile(new String[]{contentId.toString(), feedback, timecost});
-        return ResponseEntity.ok("SUCCESS");
-    }
-
-    @PostMapping("/{contentId}/feedback")
-    public ResponseEntity<?> feedbackContent(
-            @PathVariable Long contentId,
-            @RequestParam String feedback,
-            @RequestParam(required = false) String timecost
-    ) {
-//        contentService.feedbackContent(contentId, feedback);
-        CsvUtil csvUtil = new CsvUtil();
-        csvUtil.writeCsvFile(new String[]{contentId.toString(), feedback, timecost});
-        return ResponseEntity.ok("SUCCESS");
-    }
 }
