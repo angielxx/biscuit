e194d8c7f99e03e4ecafb6a2369a0701b633418f
randallkk <randallkk.sw@gmail.com>
2023-03-29 16:49:37 +0900

    S08P22A706-219 feat: UnsupportedOperationException, FileNotFoundException handler 추가

--- backend/service API/src/main/java/com/pt/biscuIT/common/exception/GlobalExceptionHandler.java
+++ backend/service API/src/main/java/com/pt/biscuIT/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.FileNotFoundException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,4 +21,16 @@ public class GlobalExceptionHandler {
 //    protected ResponseEntity handleException(Exception e) {
 //        return new ResponseEntity(BaseResponseBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
 //    }
+
+    @ExceptionHandler({UnsupportedOperationException.class})
+    protected ResponseEntity handleUnsupportedOperationException(UnsupportedOperationException e) {
+        e.printStackTrace();
+        return new ResponseEntity(BaseResponseBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({FileNotFoundException.class})
+    protected ResponseEntity handleFileNotFoundException(FileNotFoundException e) {
+        e.printStackTrace();
+        return new ResponseEntity(BaseResponseBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
