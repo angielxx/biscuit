3e37b148a34915cc2ce262329777cd6115e04590
angielxx <angielxx94@gmail.com>
2023-03-29 16:41:24 +0900

    S08P22A706-388 feat: 피드백 제출 API 연결

--- frontend/src/components/common/Modal/RecontContentModal.tsx
+++ frontend/src/components/common/Modal/RecontContentModal.tsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
-// Component
-import FeedBackButton from './FeedBackButton';
-import Button from '../Button';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 // twin macro
 import tw, { styled, css, TwStyle } from 'twin.macro';
@@ -13,10 +10,16 @@ import Close from '../../../assets/icons/close.svg';
 import ContentCardItem from '../ContentCardItem';
 import QuizItem from './QuizItem';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { getTimeSelector, recentContentState, startTimeState } from '../../../recoils/Contents/Atoms';
+import {
+  getTimeSelector,
+  recentContentState,
+  startTimeState,
+} from '../../../recoils/Contents/Atoms';
 import QuizResultPage from './QuizResultPage';
 import QuizPage from './QuizPage';
 import FeedbackPage from './FeedbackPage';
+import { post_feedback } from '../../../api/feedback';
+import { get_quizzes, post_quizzes } from '../../../api/quiz';
 
 // Styled component
 const Container = styled.div`
@@ -66,45 +69,85 @@ interface Quiz {
 // Main component
 const RecentContentModal = ({ onClose }: FeedbackModalProps) => {
   // 페이지 (0:피드백, 1:퀴즈, 2:결과)
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState<number>(0);
   // 방금 본 컨텐츠
   const [recentContent, setRecentContent] = useRecoilState(recentContentState);
+  // 퀴즈
+  const [quizzes, setQuizzes] = useState<Quiz[]>([
+    {
+      quizId: 0,
+      question: '두번째 질문',
+      multiple_choice: ['첫번째 보기', '두번째 보기', '세번째 보기'],
+      answer: 0,
+    },
+    {
+      quizId: 0,
+      question: '두번째 질문',
+      multiple_choice: [
+        '첫번째 보기입니다.',
+        '두번째 보기입니다.',
+        '세번째 보기입니다.',
+      ],
+      answer: 0,
+    },
+    {
+      quizId: 0,
+      question: '두번째 질문',
+      multiple_choice: [
+        '첫번째 보기입니다.',
+        '두번째 보기입니다.',
+        '세번째 보기입니다.',
+      ],
+      answer: 0,
+    },
+  ]);
 
   useEffect(() => {
     // API get 요청 : 퀴즈 제공
   }, []);
 
-  let title;
-  let discription;
-  switch (page) {
-    case 0:
-      title = '방금 본 컨텐츠가 어땠는지 알려주세요.';
-      discription = '피드백을 주시면 더 정확한 컨텐츠를 받아보실 수 있어요.';
-      break;
-    case 1:
-      title = '잘 이해했는지 확인해보세요.';
-      discription = '퀴즈를 풀면 대시보드에 잔디가 자라나요.';
-      break;
-    case 2:
-      title = '채점결과';
-      discription = '';
-      break;
-
-    default:
-      break;
+  interface mutateParams {
+    contentId: number;
+    feedback: number | null;
+    timecost: number;
   }
 
-  const startTime = useRecoilValue(startTimeState);
-  const time = useRecoilValue(getTimeSelector);
+  // 피드백 POST
+  const { mutate: feedbackMutate } = useMutation({
+    mutationFn: ({ contentId, feedback, timecost }: mutateParams) =>
+      post_feedback(contentId, feedback, timecost),
+  });
+
+  // 퀴즈 답 POST
+  const { mutate: quizMutate } = useMutation({
+    mutationFn: (contentId: number) => post_quizzes(contentId),
+  });
+
+  // 퀴즈 GET
+  const { data } = useQuery({
+    queryKey: ['get_quizzes', recentContent.id],
+    queryFn: () => get_quizzes(recentContent.id),
+    onSuccess: (data) => setQuizzes(data.quizzes),
+  });
+
   // 피드백 제출
   const feedbackSubmitHandler = (feedback: number | null) => {
+    // timecost 설정 필요
+    const timecost = 1;
     // API POST 요청 : 피드백 저장
+    feedbackMutate({ contentId: recentContent.id, feedback, timecost });
     onClose();
   };
 
   // 퀴즈 제출
-  const quizSubmitHandler = () => {
+  const quizSubmitHandler = (
+    firstAnswer: number | null,
+    secondAnswer: number | null,
+    thirdAnswer: number | null
+  ) => {
     // API POST 요청 : 퀴즈 제출 내역 저장
+    console.log('answer :', firstAnswer, secondAnswer, thirdAnswer);
+    console.log(firstAnswer && secondAnswer);
   };
 
   return (
@@ -118,7 +161,9 @@ const RecentContentModal = ({ onClose }: FeedbackModalProps) => {
         {page === 0 && <FeedbackPage onSubmit={feedbackSubmitHandler} />}
 
         {/* 퀴즈 */}
-        {page === 1 && <QuizPage onSubmit={quizSubmitHandler} />}
+        {page === 1 && (
+          <QuizPage onSubmit={quizSubmitHandler} quizzes={quizzes} />
+        )}
 
         {/* 퀴즈 결과 */}
         {page === 2 && <QuizResultPage />}
