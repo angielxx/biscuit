796d164d2df7c427f1bde08fa82cc4b4cc6fdc93
randallkk <randallkk.sw@gmail.com>
2023-03-29 17:49:42 +0900

    S08P22A706-201 feat: AuthController, MemberController 생성

---
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.pt.biscuIT.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+}
