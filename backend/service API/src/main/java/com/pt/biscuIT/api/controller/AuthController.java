3155b0919e5056fa9e2d97b899a78caae97da312
randallkk <randallkk.sw@gmail.com>
2023-03-31 04:06:55 +0900

    S08P22A706-201 feat: redirect-uri 설정

--- backend/service API/src/main/java/com/pt/biscuIT/api/controller/AuthController.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/AuthController.java
@@ -2,13 +2,20 @@ package com.pt.biscuIT.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @GetMapping("/callback/{provider}")
+    public void signIn(@RequestParam String code, @PathVariable String provider) {
+        log.info("code: " + code);
+        log.info("provider: " + provider);
+    }
+
+
 
 }
