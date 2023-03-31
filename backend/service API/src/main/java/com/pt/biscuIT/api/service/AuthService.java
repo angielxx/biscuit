356026d42ff5bfaa3c2f2a4df87f1fa195d6c6af
randallkk <randallkk.sw@gmail.com>
2023-03-31 18:05:11 +0900

    S08P22A706-201 feat: 오류 있는 커밋

--- backend/service API/src/main/java/com/pt/biscuIT/api/service/AuthService.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/service/AuthService.java
@@ -1,20 +1,25 @@
 package com.pt.biscuIT.api.service;
 
 import com.pt.biscuIT.api.dto.member.MemberAuthDto;
+import com.pt.biscuIT.api.dto.member.OAuthAttributes;
+import com.pt.biscuIT.db.entity.Role;
 import com.pt.biscuIT.db.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.security.core.userdetails.UserDetails;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import com.pt.biscuIT.db.entity.Member;
+
+import java.util.Map;
 
 
 /**
@@ -23,7 +28,7 @@ import com.pt.biscuIT.db.entity.Member;
 @Component
 @RequiredArgsConstructor
 public class AuthService implements OAuth2UserService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
 //    public MemberAuthDto loadMemberByEmail(String email) {
 //        Member member = memberService.getMemberByEmail(email);
@@ -33,13 +38,32 @@ public class AuthService implements OAuth2UserService {
 //        return null;
 //    }
 
-    public MemberAuthDto loadMemberByEmail(String email) throws UsernameNotFoundException {
-        return loadMemberByEmail(email);
-    }
-
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        return null;
+        DefaultOAuth2UserService service = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = service.loadUser(userRequest);
+
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        OAuthAttributes extractAttributes = OAuthAttributes.of(provider, userNameAttributeName, attributes);
+
+        Member member = saveOrUpdate(oAuth2User);
+
+        return new DefaultOAuth2User(
+
+        );
+    }
+
+    public Member saveOrUpdate(OAuth2User oAuth2User) {        String email = oAuth2User.getAttribute("email");
+        Member oAuthMember = Member.builder()
+                .provider()
+                .role(Role.BASIC)
+                .build();
+        Member member = memberRepository.findByEmail(oAuthMember.getEmail())
+                .map(entity -> entity.update(oAuthMember))
+                .orElse(oAuthMember);
+        return memberRepository.save(member);
     }
 
 }
