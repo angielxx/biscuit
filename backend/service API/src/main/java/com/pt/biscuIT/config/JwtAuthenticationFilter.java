cd126a287a4aac3f880c2d35aba05dc0d98c4aff
randallkk <randallkk.sw@gmail.com>
2023-03-31 18:05:11 +0900

    S08P22A706-201 feat: 오류 있는 커밋

--- backend/service API/src/main/java/com/pt/biscuIT/config/JwtAuthenticationFilter.java
+++ backend/service API/src/main/java/com/pt/biscuIT/config/JwtAuthenticationFilter.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.pt.biscuIT.api.dto.member.MemberAuthDto;
+import com.pt.biscuIT.api.service.AuthService;
 import com.pt.biscuIT.db.entity.Member;
 import com.pt.biscuIT.api.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -28,11 +30,12 @@ import com.pt.biscuIT.common.util.ResponseBodyWriteUtil;
  * 요청 헤더에 jwt 토큰이 있는 경우, 토큰 검증 및 인증 처리 로직 정의.
  */
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private AuthService authService;
     private MemberService memberService;
 
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, MemberService memberService) {
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, AuthService authService) {
         super(authenticationManager);
-        this.memberService = memberService;
+        this.authService = authService;
     }
 
     @Override
@@ -75,7 +78,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
             // If so, then grab user details and create spring auth token using username, pass, authorities/roles
             if (email != null) {
                 // jwt 토큰에 포함된 계정 정보(userId) 통해 실제 디비에 해당 정보의 계정이 있는지 조회.
-                Member member = memberService.getMemberByEmail(email);
+                Member member = memberService.findMemberByEmail(email);
                 if(member != null) {
                     // 식별된 정상 유저인 경우, 요청 context 내에서 참조 가능한 인증 정보(jwtAuthentication) 생성.
                     MemberAuthDto memberDetails = new MemberAuthDto(member);
