211c51bb5f0b7d687a44440180be6c965a9a6a4b
dbgksquf05 <dbgksquf05@naver.com>
2023-03-31 17:01:08 +0900

    S08P22A706-316 feat: 마이페이지 정보 컴포넌트 구현

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -3,24 +3,18 @@ import { useState } from 'react';
 import Contributions from "../components/Dashboard/Contributions";
 import Graph from "../components/Dashboard/Graph";
 import Point from "../components/Dashboard/Point";
+import MyInfos from "../components/Dashboard/MyInfos";
 
-const HomeContainer = tw.div`
-  flex-col w-screen justify-center px-6 pt-4
-`;
-
-
-const MyInfoContainer = tw.div``;
-const SettingContainer = tw.div`flex flex-col w-[20%] items-end`
-
+const HomeContainer = tw.div`flex-col w-screen justify-center px-6 pt-4`;
+const MyInfoContainer = tw.div`pb-6 mb-6 border-b-2 border-dark-grey20`;
+const SettingContainer = tw.div`flex flex-col w-[25%] items-end justify-center`
 const DashboardContainer = tw.div``;
-const HeaderContainer = tw.div`flex flex-row`;
-const DashboardHeader = tw.div`flex flex-col w-[80%]`;
-const PointContainer = tw.div`flex flex-col w-[20%] justify-around items-end`
-
-const Title = tw.p`text-white text-h2 mb-2`;
+const HeaderContainer = tw.div`flex flex-row mb-5`;
+const DashboardHeader = tw.div`flex flex-col w-[75%]`;
+const PointContainer = tw.div`flex flex-col w-[25%] justify-around items-end`
+const Title = tw.p`text-white text-h2`;
 const Span = tw.p`text-dark-grey70 text-sub`;
-
-const Logo = tw.img`w-8 h-8`;
+const Logo = tw.img`w-6 h-6`;
 
 const Setting = ({ category }: { category: string }) => {
   const imgSrc = `src/assets/icons/${category}.svg`;
@@ -51,7 +45,7 @@ export default function MyPage() {
         <HeaderContainer>
           <DashboardHeader>
             <Title>비스킷님의 정보</Title>
-            <Span>사용자 정보에 맞는 맞춤 컨텐츠를 제공해드려요.</Span>
+            {/* <Span>사용자 정보에 맞는 컨텐츠를 제공해드려요.</Span> */}
           </DashboardHeader>
           <SettingContainer onClick={() => console.log("클릭")}>
             <Setting category="setting"/>
@@ -63,7 +57,7 @@ export default function MyPage() {
         <HeaderContainer>
           <DashboardHeader>
             <Title>대시보드</Title>
-            <Span>컨텐츠를 보고 퀴즈를 풀면 잔디가 자라나요.</Span>
+            <Span>퀴즈를 풀면 잔디가 자라나요.</Span>
           </DashboardHeader>
           <PointContainer>
             <Point point={100} />
