9a2c37bb95bf0f3ddedde843aa46d968e76279cc
randallkk <randallkk.sw@gmail.com>
2023-03-31 18:04:01 +0900

    S08P22A706-201 fix: 자체 로그인 관련 설정 지우고 OAuth로 교체

--- backend/service API/src/main/java/com/pt/biscuIT/api/dto/member/MemberAuthDto.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/dto/member/MemberAuthDto.java
@@ -2,61 +2,45 @@ package com.pt.biscuIT.api.dto.member;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import com.pt.biscuIT.db.entity.Member;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 /**
  * 현재 액세스 토큰으로 부터 인증된 유저의 부가 상세정보(활성화 여부, 만료, 롤 등) 정의.
  */
+@Getter
 @RequiredArgsConstructor
-public class MemberAuthDto implements UserDetails {
+public class MemberAuthDto implements OAuth2User {
     Member member;
-    boolean accountNonExpired;
-    boolean accountNonLocked;
-    boolean credentialNonExpired;
-    boolean enabled = false;
+    Map<String, Object> attributes;
     List<GrantedAuthority> roles = new ArrayList<>();
 
-    public MemberAuthDto(Member member) {
-        super();
+    public MemberAuthDto(Member member, Map<String, Object> attributes) {
         this.member = member;
+        this.attributes = attributes;
     }
 
-    public Member getMember() {
-        return this.member;
-    }
     @Override
-    public String getPassword() {
-        return "BISCUIT NOT USE PASSWORD";
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
-    @Override
-    public String getUsername() {
-        return "BISCUIT NOT USE USER NAME";
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return this.accountNonExpired;
-    }
-    @Override
-    public boolean isAccountNonLocked() {
-        return this.accountNonLocked;
-    }
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return this.credentialNonExpired;
-    }
-    @Override
-    public boolean isEnabled() {
-        return this.enabled;
-    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.roles;
     }
     public void setAuthorities(List<GrantedAuthority> roles) {
         this.roles = roles;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
--- backend/service API/src/main/java/com/pt/biscuIT/config/SecurityConfig.java
+++ backend/service API/src/main/java/com/pt/biscuIT/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.pt.biscuIT.api.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -40,14 +41,14 @@ public class SecurityConfig {
 
     private MemberService memberService;
 
-//    인증 필요 없는뎅
-//    @Bean
-//    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//
-//    // DAO 기반으로 Authentication Provider를 생성
-//    // BCrypt Password Encoder와 UserDetailService 구현체를 설정
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    // DAO 기반으로 Authentication Provider를 생성
+    // BCrypt Password Encoder와 UserDetailService 구현체를 설정
 //    @Bean
 //    DaoAuthenticationProvider authenticationProvider() {
 //        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
@@ -58,29 +59,28 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic().disable()
-//                .csrf().disable()
-//                    .and()
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.GET, OPEN_API_GET).permitAll()
-//                .antMatchers(HttpMethod.POST, OPEN_API_POST).permitAll()
-//                .anyRequest().authenticated()
-//                    .and()
-//                .oauth2Login()          // OAuth2기반의 로그인인 경우
-//                .userInfoEndpoint()     // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
-//                .userService(authService)  // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
-//                .and().cors();
-
         http
                 .httpBasic().disable()
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
-                .and()
-//                .addFilter(new JwtAuthenticationFilter(authenticationManager(), memberService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()
-                .anyRequest().permitAll()
-                .and().cors();
+                .antMatchers(HttpMethod.GET, OPEN_API_GET).permitAll()
+                .antMatchers(HttpMethod.POST, OPEN_API_POST).permitAll()
+                .anyRequest().authenticated()
+                .and().cors()
+                    .and()
+                .oauth2Login()          // OAuth2기반의 로그인인 경우
+                .userInfoEndpoint()     // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
+                .userService(authService);  // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록;
+
+//        http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
+//                .and()
+////                .addFilter(new JwtAuthenticationFilter(authenticationManager(), memberService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+//                .authorizeRequests()
+//                .anyRequest().permitAll()
+//                .and().cors();
         return http.build();
     }
 }
