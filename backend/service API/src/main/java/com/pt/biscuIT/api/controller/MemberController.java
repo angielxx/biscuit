f453fc70e6d48cb72d0709486686f0d2adb265d5
7unho <rlawnsgh8395@naver.com>
2023-04-03 17:58:31 +0900

    feat: 대시보드 api 구현

--- backend/service API/src/main/java/com/pt/biscuIT/api/controller/MemberController.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/MemberController.java
@@ -1,20 +1,26 @@
 package com.pt.biscuIT.api.controller;
 
+import com.pt.biscuIT.api.dto.history.MemberGraphDto;
+import com.pt.biscuIT.api.dto.history.MemberHistoryDto;
+import com.pt.biscuIT.api.response.MemberDashboardRes;
 import com.pt.biscuIT.api.service.MemberAuthService;
 import com.pt.biscuIT.api.service.MemberService;
+import com.pt.biscuIT.common.model.response.BaseResponseBody;
 import com.pt.biscuIT.db.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
-    MemberService memberService;
-    MemberAuthService memberAuthService;
+    private final MemberService memberService;
+    private final MemberAuthService memberAuthService;
 
     @GetMapping("/")
     public ResponseEntity<?> getMemberByEmail(String email) {
@@ -34,4 +40,20 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/dashboard")
+    public ResponseEntity<? extends BaseResponseBody> getDashBoardByMember(String identifier) {
+        Member member = memberService.findMemberByIdentifier(identifier);
+//        if(member == null) throw new BiscuitException(ErrorCode.USER_NOT_FOUND);
+
+        List<MemberHistoryDto> histories = memberService.getHistoriesByMember(member);
+        List<MemberGraphDto> graphs = memberService.getGraphsByMember(member);
+        Integer point = memberService.getPointByMember(member);
+
+        MemberDashboardRes res = MemberDashboardRes.builder()
+                .histories(histories)
+                .graphs(graphs)
+                .point(point)
+                .build();
+        return ResponseEntity.ok(MemberDashboardRes.of(200, "Success", res));
+    }
 }
--- backend/service API/src/main/java/com/pt/biscuIT/api/service/MemberServiceImpl.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/service/MemberServiceImpl.java
@@ -1,12 +1,19 @@
 package com.pt.biscuIT.api.service;
 
+import com.pt.biscuIT.api.dto.history.MemberGraphDto;
+import com.pt.biscuIT.api.dto.history.MemberHistoryDto;
 import com.pt.biscuIT.common.exception.MemberNotFoundException;
 import com.pt.biscuIT.db.entity.Member;
+import com.pt.biscuIT.db.repository.MemberHistoryRepositorySupport;
+import com.pt.biscuIT.db.repository.MemberPointRepositorySupport;
 import com.pt.biscuIT.db.repository.MemberRepository;
 import com.pt.biscuIT.db.repository.MemberRepositorySupport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.Optional;
 
@@ -22,6 +29,9 @@ import java.util.Optional;
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final MemberRepositorySupport memberRepositorySupport;
+    private final MemberPointRepositorySupport memberPointRepositorySupport;
+
+    private final MemberHistoryRepositorySupport memberHistoryRepositorySupport;
 
     public Member findMemberById(Long id) {
         if (memberRepository.findById(id).isPresent()) {
@@ -45,5 +55,19 @@ public class MemberServiceImpl implements MemberService {
     public Member update(Member member) {
         memberRepository.save(member);
         return null;
+    }
+
+    public List<MemberHistoryDto> getHistoriesByMember(Member member) {
+        List<MemberHistoryDto> res = memberHistoryRepositorySupport.getHistoriesByMemberId(member.getId());
+        return res == null ? new ArrayList<>() : res;
+    }
+
+    public List<MemberGraphDto> getGraphsByMember(Member member) {
+        List<MemberGraphDto> res = memberHistoryRepositorySupport.getGraphsByMemberId(member.getId());
+        return res == null? new ArrayList<>() : res;
+    }
+
+    public Integer getPointByMember(Member member) {
+        return memberPointRepositorySupport.findPointByMemberId(member.getId());
     }
 }
