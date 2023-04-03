88b2251a359448bb8e93b9fc2f1fb599d4fe080c
randallkk <randallkk.sw@gmail.com>
2023-04-03 15:40:00 +0900

    S08P22A706-201 feat: onboarding API

--- backend/service API/src/main/java/com/pt/biscuIT/api/controller/MemberController.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.pt.biscuIT.api.controller;
 
+import com.pt.biscuIT.api.service.MemberAuthService;
 import com.pt.biscuIT.api.service.MemberService;
+import com.pt.biscuIT.db.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +14,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/members")
 public class MemberController {
     MemberService memberService;
+    MemberAuthService memberAuthService;
 
     @GetMapping("/")
     public ResponseEntity<?> getMemberByEmail(String email) {
-        return ResponseEntity.ok(memberService.findMemberByEmail(email));
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/signin")
-    public ResponseEntity<?> signIn(@RequestBody String tokens) {
-        System.out.println(tokens);
+    /**
+     * 회원 정보 조회
+     * @param token
+     * @return
+     */
+    @PostMapping("/onboarding")
+    public ResponseEntity<?> onboard(@RequestHeader(required = false, value = "Authorization") String token) {
+        Member member = memberAuthService.getMember(token);
+
+        memberService.update(member);
         return ResponseEntity.ok().build();
     }
 }
