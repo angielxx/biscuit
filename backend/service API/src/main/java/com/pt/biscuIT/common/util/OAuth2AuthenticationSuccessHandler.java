73b5319f1b4ea71e5c6dfb1181983fa443914a80
randallkk <randallkk.sw@gmail.com>
2023-04-03 15:39:42 +0900

    S08P22A706-201 fix: redirect-url(client)

--- backend/service API/src/main/java/com/pt/biscuIT/common/util/OAuth2AuthenticationSuccessHandler.java
+++ backend/service API/src/main/java/com/pt/biscuIT/common/util/OAuth2AuthenticationSuccessHandler.java
@@ -8,11 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +20,7 @@ import java.io.IOException;
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-    private String clientUrl = "http://localhost";
+    private String clientUrl = "http://localhost:5173";
     MemberRefreshTokenRedisRepository memberRefreshTokenRedisRepository;
 //    private RequestCache requestCache = new HttpSessionRequestCache();
 
@@ -47,18 +43,16 @@ public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthent
 //        // Use the DefaultSavedRequest URL
 //        String targetUrl = savedRequest.getRedirectUrl();
 
-//        clientUrl = targetUrl.substring(0, targetUrl.indexOf("/login/oauth2/code/"));
-        // 내가 추가한 로직: access token, refresh token 발급
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
 
-        String accesstoken = JwtTokenUtil.getToken(JwtTokenUtil.ACCESS_TOKEN_NAME, oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() + oAuth2User.getName());
-        String refreshtoken = JwtTokenUtil.getToken(JwtTokenUtil.REFRESH_TOKEN_NAME, oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() + oAuth2User.getName());
+        String accesstoken = JwtTokenUtil.createToken(JwtTokenUtil.ACCESS_TOKEN_NAME, oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() + oAuth2User.getName());
+        String refreshtoken = JwtTokenUtil.createToken(JwtTokenUtil.REFRESH_TOKEN_NAME, oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() + oAuth2User.getName());
         MemberRefreshToken memberRefreshToken = new MemberRefreshToken(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() + oAuth2User.getName(), refreshtoken, JwtTokenUtil.getExpiration(refreshtoken));
 //        memberRefreshTokenRedisRepository.save(memberRefreshToken);
         response.addCookie(CookieUtil.createCookie(clientUrl, JwtTokenUtil.ACCESS_TOKEN_NAME, accesstoken));
         response.addCookie(CookieUtil.createCookie(clientUrl, JwtTokenUtil.REFRESH_TOKEN_NAME, refreshtoken));
-        getRedirectStrategy().sendRedirect(request, response, clientUrl + ":3000/login/oauth2/redirect");
+        getRedirectStrategy().sendRedirect(request, response, clientUrl + "/login/oauth2/redirect");
     }
 
 
