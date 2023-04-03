0f66cd2b88898308f2a72f87be0f118acf52fdb6
randallkk <randallkk.sw@gmail.com>
2023-04-03 16:50:25 +0900

    S08P22A706-201 fix: token을 header와 cookie 둘 다 박아보자

--- backend/service API/src/main/java/com/pt/biscuIT/common/util/CookieUtil.java
+++ backend/service API/src/main/java/com/pt/biscuIT/common/util/CookieUtil.java
@@ -8,9 +8,9 @@ public class CookieUtil {
     public static Cookie createCookie(String clientUrl, String name, String value) {
         Cookie cookie = new Cookie(name, value);
 //        cookie.setDomain(clientUrl);
-//        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(true);
         cookie.setPath("/");
-//        cookie.setSecure(true);
+        cookie.setSecure(true);
         cookie.setMaxAge(3600);
         return cookie;
     }
