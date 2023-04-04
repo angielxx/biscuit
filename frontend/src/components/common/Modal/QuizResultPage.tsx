664450ff61ea08a52f3160ae91a2f0d195c7e4e4
angielxx <angielxx94@gmail.com>
2023-04-04 14:33:11 +0900

    S08P22A706-307 feat: 퀴즈 결과 페이지 구현

--- frontend/src/components/common/Modal/QuizResultPage.tsx
+++ frontend/src/components/common/Modal/QuizResultPage.tsx
@@ -13,36 +13,46 @@ interface Quiz {
 
 interface QuizResultPageProps {
   quizzes: Quiz[];
+  userAnswers: number[];
 }
 
-const QuizResultPage = ({ quizzes }: QuizResultPageProps) => {
+const QuizResultPage = ({ quizzes, userAnswers }: QuizResultPageProps) => {
   // 3가지 퀴즈 각각 저장
   const [firstQuiz, setFirstQuiz] = useState<Quiz>(quizzes[0]);
   const [secondQuiz, setSecondQuiz] = useState<Quiz>(quizzes[1]);
   const [thirdQuiz, setThirdQuiz] = useState<Quiz>(quizzes[2]);
   // 선택한 퀴즈 정답
-  const [firstAnswer, setFirstAnswer] = useState<null | number>(null);
-  const [secondAnswer, setSecondAnswer] = useState<null | number>(null);
-  const [thirdAnswer, setThirdAnswer] = useState<null | number>(null);
+  const [firstAnswer, setFirstAnswer] = useState<number>(99);
+  const [secondAnswer, setSecondAnswer] = useState<number>(99);
+  const [thirdAnswer, setThirdAnswer] = useState<number>(99);
 
   return (
     <div>
       <PageTitle title="채점결과" desc="결과를 확인해보세요." />
-      <div className="flex flex-col gap-6 py-6">
+      <div className="flex flex-col gap-6 pt-6">
         <QuizItem
           question={firstQuiz.question}
           options={firstQuiz.multiple_choice}
           onClick={setFirstAnswer}
+          result={true}
+          userAnswer={userAnswers[0]}
+          answer={firstQuiz.answer}
         />
         <QuizItem
           question={secondQuiz.question}
           options={secondQuiz.multiple_choice}
           onClick={setSecondAnswer}
+          result={true}
+          userAnswer={userAnswers[1]}
+          answer={secondQuiz.answer}
         />
         <QuizItem
           question={thirdQuiz.question}
           options={thirdQuiz.multiple_choice}
           onClick={setThirdAnswer}
+          result={true}
+          userAnswer={userAnswers[2]}
+          answer={thirdQuiz.answer}
         />
       </div>
     </div>
