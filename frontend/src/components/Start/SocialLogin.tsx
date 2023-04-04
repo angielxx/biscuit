506a0b3e607fef8853a9af5b0cca91b071902751
yjp8842 <yjp8842@naver.com>
2023-04-04 18:07:19 +0900

    S08P22A706-134 fix: 로그인 관련 로직 수정 중

--- frontend/src/components/Start/SocialLogin.tsx
+++ frontend/src/components/Start/SocialLogin.tsx
@@ -1,6 +1,7 @@
 import { isNameState, isNoobState } from "../../recoils/Start/Atoms";
 import { useRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const SocialLogin = () => {
   const navigate = useNavigate();
@@ -15,17 +16,19 @@ const SocialLogin = () => {
   console.log(isNoob);
   console.log(nickname);
 
-  // 뉴비가 아니면
-  if (isNoob === "false") {
-    console.log("login 안됨");
-    navigate(`/`);
-    setNoob(false);
-    // setIsName(nickname);
-  } else {
-    console.log("온보딩");
-    // 뉴비이면
-    navigate(`/onboarding`);
-  }
+  useEffect(() => {
+    // 뉴비가 아니면
+    if (isNoob === "false") {
+      console.log("login 안됨");
+      navigate(`/`);
+      setNoob(false);
+      // setIsName(nickname);
+    } else {
+      console.log("온보딩");
+      // 뉴비이면
+      navigate(`/onboarding`);
+    }
+  }, [])
 
   return (
     <></>
