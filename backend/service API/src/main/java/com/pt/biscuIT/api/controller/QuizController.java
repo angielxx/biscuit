90af5918528788eea4d3d8561feae3d2357f962e
이지영 <jylisa0612@naver.com>
2023-04-04 16:44:05 +0900

    S08P22A706-280 feat: 퀴즈 제공 구현중

--- backend/service API/src/main/java/com/pt/biscuIT/api/controller/QuizController.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/QuizController.java
@@ -2,11 +2,15 @@ package com.pt.biscuIT.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pt.biscuIT.api.dto.quiz.ProvideQuizDto;
+import com.pt.biscuIT.api.service.MemberAuthService;
+import com.pt.biscuIT.api.service.MemberService;
 import com.pt.biscuIT.api.service.QuizService;
+import com.pt.biscuIT.db.entity.Member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +21,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api")
 public class QuizController {
 	private final QuizService quizService;
+	private final MemberAuthService memberAuthService;
 
 	@GetMapping("{contentId}/quizzes")
-	public ProvideQuizDto provideQuiz(@PathVariable Long contentId) {
+	public ProvideQuizDto provideQuiz(@RequestHeader(required = false, value = "Authorization") String token, @PathVariable Long contentId) {
+		Member member = memberAuthService.getMember(token);
+
 		return quizService.provideQuiz(contentId);
 	}
 
