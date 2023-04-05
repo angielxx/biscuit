480347c809ac1ab6668cf4e803359a5ddfd58633
dbgksquf05 <dbgksquf05@naver.com>
2023-04-05 13:21:38 +0900

    S08P22A706-376 feat: 홈 로그인 분기처리

--- frontend/src/recoils/Start/Atoms.ts
+++ frontend/src/recoils/Start/Atoms.ts
@@ -1,4 +1,7 @@
 import { atom } from 'recoil';
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const isStartModalState = atom({
   key: 'isStartModalState',
@@ -8,11 +11,13 @@ const isStartModalState = atom({
 const isNoobState = atom({
   key: 'isNoobState',
   default: true,
+  effects_UNSTABLE: [persistAtom],
 })
 
 const isNameState = atom({
   key: 'isNameState',
   default: "",
+  effects_UNSTABLE: [persistAtom],
 })
 
 export { 
