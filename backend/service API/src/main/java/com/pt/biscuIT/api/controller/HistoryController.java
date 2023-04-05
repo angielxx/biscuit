55648706018464eb8229ba2ab0159f4da820e3fd
이지영 <jylisa0612@naver.com>
2023-04-05 14:21:48 +0900

    S08P22A706-290 feat: 히스토리 조회 구현

--- backend/service API/src/main/java/com/pt/biscuIT/api/controller/HistoryController.java
+++ backend/service API/src/main/java/com/pt/biscuIT/api/controller/HistoryController.java
@@ -1,12 +1,17 @@
 package com.pt.biscuIT.api.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pt.biscuIT.api.response.HistoryContentRes;
 import com.pt.biscuIT.api.service.MemberAuthService;
 import com.pt.biscuIT.api.service.MemberHistoryService;
 import com.pt.biscuIT.db.entity.Member;
@@ -22,6 +27,14 @@ import lombok.extern.slf4j.Slf4j;
 public class HistoryController {
 	private final MemberAuthService memberAuthService;
 	private final MemberHistoryService memberHistoryService;
+
+	@GetMapping
+	public HistoryContentRes getHistory(@RequestHeader(value = "Authorization") String token,
+							@RequestParam(defaultValue = "999999") Long lastContentId,
+							@PageableDefault(size = 30, page = 0) Pageable pageable) {
+		Member member = memberAuthService.getMember(token);
+		return memberHistoryService.getHistory(member, lastContentId, pageable);
+	}
 
 	@PostMapping("/{historyId}")
 	public void deleteHistory(@RequestHeader(value = "Authorization") String token, @PathVariable Long historyId) {
