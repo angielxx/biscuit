76af05c9aa541a3c87884b67bbe873e5f2a23b5b
hanbeulYou <hanbeul.you@gmail.com>
2023-04-06 04:44:31 +0900

    S08P22A706-428 feat: 로그아웃 기능 구현

---
+++ frontend/src/components/common/Logout.tsx
@@ -0,0 +1,35 @@
+// css
+import tw from 'twin.macro';
+import { useSetRecoilState } from 'recoil';
+import { isNoobState } from '../../recoils/Start/Atoms';
+import { useMutation } from '@tanstack/react-query';
+import { post_signout } from '../../api/logout';
+
+const Container = tw.div`absolute bottom-0 h-14 w-[calc(100% - 16px)] px-2 border-t border-solid border-dark-evaluated flex justify-end items-center`; 
+const Btn = tw.button`w-7 h-6 mr-4`
+const Img = tw.img`w-full h-full`;
+
+export default function Logout() {
+  const setIsNoob = useSetRecoilState(isNoobState)
+
+  const { mutate: signOutMutate } = useMutation({
+    mutationFn: () => post_signout(),
+    onSuccess: () => {
+      document.cookie = "access-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      document.cookie = "refresh-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    },
+  });
+
+  const onClick = () => {
+    setIsNoob(true);
+    signOutMutate();
+  }
+
+  return (
+    <Container>
+      <Btn onClick={() => onClick()}>
+        <Img src="assets/icons/logout.svg" />
+      </Btn>
+    </Container>
+  )
+}
