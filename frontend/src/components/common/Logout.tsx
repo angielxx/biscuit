c589bafb5d8b60a5b9e682be9cdcc9b2ebcc17a2
yjp8842 <yjp8842@naver.com>
2023-04-06 13:10:22 +0900

    S08P22A706-389 fix: 쿠키 삭제 로직 수정

--- frontend/src/components/common/Logout.tsx
+++ frontend/src/components/common/Logout.tsx
@@ -4,6 +4,7 @@ import { useSetRecoilState } from 'recoil';
 import { isNoobState } from '../../recoils/Start/Atoms';
 import { useMutation } from '@tanstack/react-query';
 import { post_signout } from '../../api/logout';
+import { removeCookie } from 'typescript-cookie';
 
 const Container = tw.div`absolute bottom-0 h-14 w-[calc(100% - 16px)] p-2 border-t border-solid border-dark-evaluated flex justify-end items-center`; 
 const Btn = tw.button`w-6 h-5`
@@ -15,8 +16,10 @@ export default function Logout() {
   const { mutate: signOutMutate } = useMutation({
     mutationFn: () => post_signout(),
     onSuccess: () => {
-      document.cookie = "access-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-      document.cookie = "refresh-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      // document.cookie = "access-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      // document.cookie = "refresh-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      removeCookie('access-token');
+      removeCookie('refresh-token');
     },
   });
 
