6cec40199a739632d8ab240cb1bf0a80e24cafa2
hanbeulYou <hanbeul.you@gmail.com>
2023-04-07 02:11:28 +0900

    S08P22A706-428 feat: 관심사 수정시 홈페이지 API 재요청

--- frontend/src/pages/EditProfile.tsx
+++ frontend/src/pages/EditProfile.tsx
@@ -1,13 +1,14 @@
 import tw from "twin.macro";
 import Button from "../components/common/Button";
 import EditInfo from "../components/Dashboard/EditInfo";
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState, useEffect } from "react";
 import { put_myInfo } from "../api/editProfile";
 import { useNavigate } from "react-router-dom";
 import { get_myInfo } from "../api/myInfo";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { isNameState } from "../recoils/Start/Atoms";
+import { homeFilterBtnState, homeFilterTimeState } from "../recoils/Home/Atoms";
 
 const HomeContainer = tw.div`flex-col w-screen justify-center px-6 pt-4`;
 const MyInfoContainer = tw.div`pb-6 mb-6`;
@@ -47,15 +48,34 @@ export default function EditProfile() {
     interests: [""],
   });
 
+  // query 재요청 로직 추가 : 한별
+  type filterItem = {
+    id: number;
+    content: string;
+    status: boolean;
+  };
+  const queryClient = useQueryClient();
+  const timeFilter = useRecoilValue(homeFilterTimeState);
+  const [timeFilterIdx, setTimeFilterIdx] = useState(6);
+  const typeFilter = useRecoilValue(homeFilterBtnState);
+
+  useEffect(() => {
+    let timeIdx: number = 6;
+    timeFilter.forEach((time: filterItem) => {
+      if (time.status === true) timeIdx = time.id;
+    });
+    setTimeFilterIdx(timeIdx);
+  }, [timeFilter]);
+
   const { mutate: signOutMutate } = useMutation({
     mutationFn: () => put_myInfo(userData),
-    onSuccess: () => {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(['get_personal_contents', "bookmarked", timeFilterIdx, typeFilter]);
       navigate('/mypage');
     },
   });
 
   const setIsNameState = useSetRecoilState(isNameState);
-
   const onSubmit = () => {
     setIsNameState(userData.nickname);
     signOutMutate();
