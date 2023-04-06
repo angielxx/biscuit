e52bf3f5fd8523a8f32ae20680f69cbc3e660f36
angielxx <angielxx94@gmail.com>
2023-04-06 14:41:26 +0900

    S08P22A706-297 feat: 북마크 추가, 삭제 API 연결

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -16,7 +16,10 @@ const authAPI = (url: string, options?: any) => {
 // config의 any 타입은 임시
 const setTokenHeader = (config: any) => {
   // 쿠키에 담긴 토큰 가져오기
-  const token = getCookie('access-token');
+  // const token = getCookie('access-token');
+  // 임시 토큰
+  const token =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnb29nbGUxMTQ2OTQ1NTc4ODcwMDI1MzMyOTYiLCJpc3MiOiJiaXNjdWl0IiwiZXhwIjoxNjgwNzY4NTQwLCJpYXQiOjE2ODA3NTc3NDB9.sbZSfU9wN-hbESwm_2h1wG-0ClN98JO5jDH-Ba1YfqU2UDSKRivi3NP3b1EbThao6eadhAU5QTgCIDWid6tUcQ';
 
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
