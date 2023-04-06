fa8dd0171f16f37cf3efde003bf5ce87d79711cb
yjp8842 <yjp8842@naver.com>
2023-04-06 15:44:21 +0900

    S08P22A706-389 feat: 사이드바 백드롭 막기

--- frontend/src/components/common/Asidebar.tsx
+++ frontend/src/components/common/Asidebar.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { isNameState, isStartModalState } from '../../recoils/Start/Atoms';
 import { isNoobState } from '../../recoils/Start/Atoms';
+import ReactDOM from 'react-dom';
 
 // components
 import BigCategory from './BigCategory';
@@ -43,6 +44,21 @@ const Aside = styled.div`
 const Closeicon = styled.div`
   ${tw`flex justify-end items-start p-2 self-stretch`}
 `;
+
+const BackdropWrapper = styled.div`
+  ${css`
+    background: rgba(0, 0, 0, 0.75);
+  `}
+  ${tw`fixed top-0 left-0 w-full h-full z-20`}
+`;
+
+interface BackdropProps {
+  onClose?: () => void;
+}
+
+function Backdrop({ onClose }: BackdropProps) {
+  return <BackdropWrapper onClick={onClose} />;
+}
 
 interface AsidebarStatus {
   isOpen: boolean;
@@ -86,49 +102,54 @@ const Asidebar = ({ isOpen, setIsOpen }: AsidebarStatus) => {
   };
 
   return (
-    <Aside className={isOpen ? 'open' : ''}>
-      <Closeicon>
-        <img
-          src={close}
-          alt="close"
-          onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
-        />
-      </Closeicon>
+    <>
+      <Backdrop onClose={() => setIsOpen(false)} />
 
-      {isNoob === false ? (
-        <AsideProfile isName={isName} onClick={goToMypage} setIsOpen={setIsOpen} />
-      ) : (
-        <AsideLogin onClick={startModal} />
-      )}
-
-      {data?.map((item, index) => {
-        return (
-          <BigCategory
-            key={item.id}
-            isCategory={page === index ? isCategory : false}
-            isClicked={isClicked}
-            item={item}
-            onClick={() => {
-              setPage(index);
-              // page !== index 일 경우, isCategory가 true면, 걍 true로 냅둬야한다.
-              isCategory
-                ? page === index
-                  ? setIsCategory(false)
-                  : setIsCategory(true)
-                : setIsCategory(true);
-            }}
-            selectList={[]}
-            locate="aside"
+      <Aside className={isOpen ? 'open' : ''}>
+        <Closeicon>
+          <img
+            src={close}
+            alt="close"
+            onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
           />
-        );
-      })}
-      
-      {/* 로그아웃 버튼 좀 추가할게 고마워 */}
-      {isNoob === false 
-        ? <Logout />
-        : null 
-      }
-    </Aside>
+        </Closeicon>
+
+        {isNoob === false ? (
+          <AsideProfile isName={isName} onClick={goToMypage} setIsOpen={setIsOpen} />
+        ) : (
+          <AsideLogin onClick={startModal} />
+        )}
+
+        {data?.map((item, index) => {
+          return (
+            <BigCategory
+              key={item.id}
+              isCategory={page === index ? isCategory : false}
+              isClicked={isClicked}
+              item={item}
+              onClick={() => {
+                setPage(index);
+                // page !== index 일 경우, isCategory가 true면, 걍 true로 냅둬야한다.
+                isCategory
+                  ? page === index
+                    ? setIsCategory(false)
+                    : setIsCategory(true)
+                  : setIsCategory(true);
+              }}
+              selectList={[]}
+              locate="aside"
+            />
+          );
+        })}
+        
+        {/* 로그아웃 버튼 좀 추가할게 고마워 */}
+        {isNoob === false 
+          ? <Logout />
+          : null 
+        }
+      </Aside>
+    
+    </>
   );
 };
 
